11c3e49e4a6d1301f4ea4065ea2ac317c8e75fe4
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-05-07 15:27:40 +0900

    Update 20220709_echo_interrupt.c

--- HW4/20220709_echo_interrupt.c
+++ HW4/20220709_echo_interrupt.c
@@ -2,7 +2,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/signal.h>
 #include <termios.h>
 #include <fcntl.h>
 #include <errno.h>
@@ -11,7 +10,7 @@
 #define BAUDRATE B1000000
 
 int fd;
-char buf[256];
+char buf[256];  // 버퍼 사이즈 정의
 
 int pin_num[] = { 29, 28, 23, 22, 21, 27, 26 };
 
@@ -37,14 +36,13 @@ void updateLEDs(char firstChar) {
     };
 
     int index = (firstChar >= '0' && firstChar <= '9') ? firstChar - '0' :
-        (firstChar >= 'A' && firstChar <= 'F') ? firstChar - 'A' + 10 : -1;
+                (firstChar >= 'A' && firstChar <= 'F') ? firstChar - 'A' + 10 : -1;
 
     if (index != -1) {
         for (int i = 0; i < PIN_COUNT; i++) {
             digitalWrite(pin_num[i], hex_table[index][i]);
         }
-    }
-    else {
+    } else {
         // Display 'X' for invalid input
         digitalWrite(29, 0);
         digitalWrite(28, 1);
@@ -56,32 +54,18 @@ void updateLEDs(char firstChar) {
     }
 }
 
-void callback_function(int status) {
-    tcflush(fd, TCIFLUSH); // Clear any residual data
-    int cnt = read(fd, buf, 256);
-    if (cnt > 0) {
-        buf[cnt] = '\0';
-        write(fd, "echo: ", 6);
-        write(fd, buf, cnt);
-        write(fd, "\r\n", 2);
-        printf("Received: %s\r\n", buf);
-        updateLEDs(buf[0]);
-    }
-}
-
 void task() {
     int i;
     for (i = 0; i < 400000000; i++);
 }
 
 int main() {
-    wiringPiSetup(); // Setup the WiringPi library
+    wiringPiSetup();  // Setup the WiringPi library
     for (int i = 0; i < 7; i++) {
         pinMode(pin_num[i], OUTPUT);
     }
 
     struct termios newtio;
-    struct sigaction saio;
 
     fd = open("/dev/serial0", O_RDWR | O_NOCTTY);
     if (fd < 0) {
@@ -89,13 +73,7 @@ int main() {
         return -1;
     }
 
-    memset(&saio, 0, sizeof(saio));
-    saio.sa_handler = callback_function;
-    sigaction(SIGIO, &saio, NULL);
-
-    fcntl(fd, F_SETOWN, getpid());
-    fcntl(fd, F_SETFL, FASYNC);
-
+    memset(&newtio, 0, sizeof(newtio));
     newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
     newtio.c_iflag = ICRNL;
     newtio.c_oflag = 0;
@@ -104,12 +82,21 @@ int main() {
     newtio.c_cc[VMIN] = 1;
 
     tcsetattr(fd, TCSANOW, &newtio);
-    tcflush(fd, TCIFLUSH);
+    tcflush(fd, TCIFLUSH);  // Flush old data
 
     write(fd, "Interrupt method\r\n", 25);
 
     while (1) {
-        task(); // Continuous background task
+        task();
+        memset(buf, 0, sizeof(buf));  // Clear buffer before reading
+        int cnt = read(fd, buf, sizeof(buf) - 1);
+        if (cnt > 0) {
+            buf[cnt] = '\0';  // Ensure null-terminated string
+            write(fd, "Echo: ", 6);
+            write(fd, buf, cnt);
+            write(fd, "\r\n", 2);
+            updateLEDs(buf[0]);
+        }
     }
 
     close(fd);
