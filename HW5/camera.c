caebb36bd4010daa56da6dbe0d1faa9ab4b231f8
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-05-19 20:49:25 +0900

    Create camera.c

---
+++ HW5/camera.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <termios.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define BAUDRATE B1000000
+
+int main()
+{
+	int fd;
+	struct termios newtio;
+	char fbuf[1024];
+	char buf[256];
+
+	fd = open("/dev/serial0", O_RDWR|O_NOCTTY);
+	if(fd<0) {
+		fprintf(stderr, "failed to open port: %s.\r\n", strerror(errno));
+		printf("Make sure you are executing in sudo.\r\n");
+	}
+	usleep(250000);
+
+	memset(&newtio, 0, sizeof(newtio));
+	newtio.c_cflag = BAUDRATE|CS8|CLOCAL|CREAD;
+	newtio.c_iflag = ICRNL;
+	newtio.c_oflag = 0;
+	newtio.c_lflag = 0;
+	newtio.c_cc[VTIME] = 0;
+	newtio.c_cc[VMIN] = 1;
+	
+//	speed_t baudRate = B1000000;
+//	cfsetispeed(&newtio, baudRate);
+//	cfsetospeed(&newtio, baudRate);
+
+	tcflush(fd, TCIFLUSH);
+	tcsetattr(fd, TCSANOW, &newtio);
+
+	while(1) {
+
+		// Insert your code
+
+	}
+	return 0;
+}
