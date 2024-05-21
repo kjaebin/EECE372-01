d615088c8590fbb7695d92b1e9be8d4645eb4dce
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-05-21 09:27:36 +0900

    Update camera.c

--- HW5/camera.c
+++ HW5/camera.c
@@ -15,7 +15,7 @@ int main()
 	struct termios newtio;
 	char fbuf[1024];
 	char buf[256];
-
+	
 	fd = open("/dev/ttyAMA0", O_RDWR|O_NOCTTY);
 	if(fd<0) {
 		fprintf(stderr, "failed to open port: %s.\r\n", strerror(errno));
@@ -37,23 +37,29 @@ int main()
 
 	tcflush(fd, TCIFLUSH);
 	tcsetattr(fd, TCSANOW, &newtio);
-
+	
 	while(1) {
-		int cnt = read(fd, buf, 256);
-		if (cnt > 0){
-		buf[cnt] = '\0';
-		printf("Received: %s\r\n",buf);
-		if (strcmp(buf, "c") == 0 || strcmp(buf, "C") == 0 ) {
-			system("raspistill -w 280 -h 280 -t 1 -o test.bmp");
-			FILE* file = fopen("test.bmp", "rb");
-			while(!feof(file)){
-			int cntbp = fread(fbuf,1 ,1024, file);
-			write(fd, fbuf, cntbp);
+		printf("Start!\r\n");
+		int input = read(fd, buf, sizeof(buf));
+		
+		if (input > 0) {
+			if (buf[0] == 'c' || buf[0] == 'C') {
+			system("raspistill -w 640 -h 480 -t 10 -o output.bmp");
+				char filename[20];
+				sprintf(filename, "output.bmp");
+					FILE* imageFile = fopen(filename, "rb");
+					if (imageFile) {
+						while (!feof(imageFile)) { 
+							int size = 1;
+							int Read = fread(fbuf, size, sizeof(fbuf), imageFile);
+							int Sent = write(fd, fbuf, Read);
+						}
+						printf("Success!\r\n");
+						fclose(imageFile);
+						break;
+					}			
 			}
-			fclose(file);
-		}
 		}
 	}
 	return 0;
 }
-
