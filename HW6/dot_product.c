bb3aad9cbaca8746464da163d108a6661b0bebf5
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-05-30 03:31:35 +0900

    Update dot_product.c

--- HW6/dot_product.c
+++ HW6/dot_product.c
@@ -4,7 +4,7 @@
 
 #include "omp.h"
 
-#define ARRAY_SIZE 100000
+#define ARRAY_SIZE 1000000
 
 double dotp(double *x, double *y);
 double dotp_omp(double *x, double *y);
@@ -15,13 +15,11 @@ int main() {
     double global_sum_ref = 0;
     double *x = malloc(sizeof(double) * ARRAY_SIZE);
     double *y = malloc(sizeof(double) * ARRAY_SIZE);
-    double *z = malloc(sizeof(double) * ARRAY_SIZE);
 
     clock_t start, end;
 
     srand((unsigned)time(NULL));
     for (int i = 0; i < ARRAY_SIZE; i++) {
-        z[i] = 0;
         x[i] = rand() % 10 + 1;
         y[i] = rand() % 10 + 1;
     }
@@ -48,7 +46,6 @@ int main() {
 
     free(x);
     free(y);
-    free(z);
     return 0;
 }
 
@@ -62,24 +59,19 @@ double dotp(double *x, double *y) {
     return global_sum;
 }
 
-double dotp_omp(double* x, double* y) {
+double dotp_omp(double *x, double *y) {
     omp_set_num_threads(6);
 
     double global_sum = 0.0;
 
 #pragma omp parallel
     {
-        int num_thread = omp_get_num_threads();
-        int thread_ID = omp_get_thread_num();
         double local_sum = 0.0;
-
-        // 각 쓰레드가 병렬로 작업 수행
 #pragma omp for
         for (int i = 0; i < ARRAY_SIZE; i++) {
             local_sum += x[i] * y[i];
         }
 
-        // 데이터 레이스 방지를 위해 임계 영역 사용
 #pragma omp critical
         {
             global_sum += local_sum;
@@ -88,4 +80,3 @@ double dotp_omp(double* x, double* y) {
 
     return global_sum;
 }
-
