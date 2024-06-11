dbb6a6b0d010349b8d6964f1b0e59e897454ea00
alexkim <167426870+kjaebin@users.noreply.github.com>
2024-06-11 18:13:15 +0900

    Update 20220709.c

--- HW7/20220709.c
+++ HW7/20220709.c
@@ -57,6 +57,8 @@ typedef struct _model {
     float fc_bias[FC_OUT];
 } model;
 
+int pin_num[] = { 29, 28, 23, 22, 21, 27, 26 };
+
 void resize_280_to_28(unsigned char* in, unsigned char* out);
 void Gray_scale(unsigned char* feature_in, unsigned char* feature_out);
 void Normalized(unsigned char* feature_in, float* feature_out);
